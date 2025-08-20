79ee150a43f64ccd3d0d2ea45184fa1ac1dfe727
solomonTheOne <iamstevesolomon@gmail.com>
2025-08-20 10:36:22 +0100

    all set

--- django_project/settings.py
+++ django_project/settings.py
@@ -90,13 +90,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+""""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
