7b5fde4ec989c34ed295042c559660bbe8373ef9
solomonTheOne <iamstevesolomon@gmail.com>
2025-08-20 14:09:37 +0100

    added allowed host link

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 #ALLOWED_HOSTS = [".onrender.com"]
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'my-django-app-loxo.onrender.com']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'my-barber-site.onrender.com']
 #ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
